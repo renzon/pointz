157044fcdf2a02f926bcb6d9f59322c70d6af54a
renzon <renzon@gmail.com>
2017-11-14 19:20:48 -0200

    Implementado schema da tabela transactions

--- pointz/bigquery/schema.py
+++ pointz/bigquery/schema.py
@@ -1,5 +1,33 @@
+from google.api_core.exceptions import NotFound
+from google.cloud.bigquery import Table, SchemaField
+
 from pointz.bigquery.client import client
 
-dataset_pointz=client.dataset('pointz')
+dataset_pointz = client.dataset('pointz')
 
 print(dataset_pointz)
+
+_transactions_ref = dataset_pointz.table('transactions')
+try:
+    transactions_table = client.get_table(_transactions_ref)
+except NotFound:
+    transactions_table = Table(_transactions_ref)
+    _schema = [
+        SchemaField('id', 'INT64', 'REQUIRED', None),
+        SchemaField('sale', 'INT64', 'REQUIRED', None),
+        SchemaField('pointz_sale', 'INT64', 'REQUIRED', None),
+        SchemaField('year', 'INT64', 'REQUIRED', None),
+        SchemaField('month', 'INT64', 'REQUIRED', None),
+        SchemaField('day', 'INT64', 'REQUIRED', None),
+        SchemaField('store_name', 'string', 'REQUIRED', None),
+        SchemaField('store_id', 'INT64', 'REQUIRED', None),
+        SchemaField('region_name', 'string', 'REQUIRED', None),
+        SchemaField('region_id', 'INT64', 'REQUIRED', None),
+        SchemaField('client_name', 'string', 'REQUIRED', None),
+        SchemaField('client_id', 'INT64', 'REQUIRED', None),
+        SchemaField('segment_name', 'string', 'REQUIRED', None),
+    ]
+
+    transactions_table.schema = _schema
+    transactions_table = client.create_table(transactions_table)
+
